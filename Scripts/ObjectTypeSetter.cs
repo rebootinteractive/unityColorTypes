6c3c72ac0691819136ed0cb3dfaa1677657a1e26
Umur Can <>
2025-09-24 16:29:46 +0300

    Default type option to setter

--- Scripts/ObjectTypeSetter.cs
+++ Scripts/ObjectTypeSetter.cs
@@ -9,6 +9,7 @@ namespace ObjectType
     {
         public ObjectTypeEnum[] objectTypes;
         public bool randomize;
+        public bool setDefaultType;
 
 #if ODIN_INSPECTOR
         [Button]
@@ -21,7 +22,15 @@ namespace ObjectType
             for (int i = 0; i < children.Length; i++)
             {
                 ObjectTypeController child = children[i];
-                child.SetObjectType(objectTypes[randomize ? Random.Range(0, objectTypes.Length) : i % objectTypes.Length]);
+                var type = objectTypes[randomize ? Random.Range(0, objectTypes.Length) : i % objectTypes.Length];
+                if (setDefaultType)
+                {
+                    child.defaultType = type;
+                }
+                child.SetObjectType(type);
+#if UNITY_EDITOR
+                UnityEditor.EditorUtility.SetDirty(child);
+#endif
             }
         }
 
